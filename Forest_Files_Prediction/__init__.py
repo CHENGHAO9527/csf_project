7ab658a6094dd1f9e4f9a3464f6bc055cc30fe71
Hao Cheng <ch343891025@gmail.com>
2018-12-11 15:42:20 -0500

    create the init file

--- Forest_Files_Prediction/__init__.py
+++ Forest_Files_Prediction/__init__.py
@@ -1,1 +1,10 @@
 
+"""csf_project
+We plan to use a data science approach to analyzing the forest fire data
+in order to predict the occurence of burned area created by a forest fire, based on meteorological data.
+"""
+
+__author__ =  "Jiajun Wu, Cheng Miao, Hao Cheng"
+__version__ = '0.0'
+__status__ = 'prototype'
+
