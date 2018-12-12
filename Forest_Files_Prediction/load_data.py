300ba82cc107ab3ad1b9239fe0e421bc4f094f44
CHENGHAO9527 <45441477+CHENGHAO9527@users.noreply.github.com>
2018-12-12 15:23:44 -0500

    Delete load_data.py

--- Forest_Files_Prediction/load_data.py
+++
@@ -1,9 +0,0 @@
-import warnings
-warnings.filterwarnings("ignore")
-import pandas as pd
-
-# Load the data
-original_df = pd.read_csv('http://www.dsi.uminho.pt/~pcortez/forestfires/forestfires.csv')
-
-# Show the header and the first five rows
-original_df.head()
